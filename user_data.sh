8b2cba0dd41083e9542c8c60eb7556fffa430742
FabioVerasMachado <fabioverasmachado@gmail.com>
2024-10-27 09:12:58 -0300

    Complementando

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Instalação da instância - Docker
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl start docker
@@ -13,3 +14,16 @@ sudo mv /usr/local/bin/docker-compose /bin/docker-compose
 sudo yum -y install amazon-efs-utils
 sudo mkdir /mnt/efs/
 sudo chmod +rwx /mnt/efs/
+
+# Executando contêineres via Docker Compose
+sudo yum install git -y
+cd /mnt/efs
+git clone https://github.com/alexlsilva7/atividade_aws_docker.git /home/ec2-user/atividade_aws_docker
+# Subir os contêineres
+docker-compose up -d
+
+# adicionar o EFS no fstab
+echo "IP_OU_DNS_DO_NFS:/ /mnt/nfs nfs defaults 0 0" >> /etc/fstab
+# montar o EFS
+sudo umount /mnt/efs
+sudo mount -a
